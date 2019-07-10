1b4e2143f78056213617a5a63efe85252fb6c3b2
P'Best <chaiwat.sunrit@gmail.com>
2019-07-10 10:55:30 +0700

    edit spec

--- py-app.py
+++ py-app.py
@@ -7,60 +7,86 @@ from pylibra.transaction import TransferTransaction
 app = Flask(__name__)
 
 
-@app.route('/libra/registerAccount', methods=['POST'])
-def registerAccount():
+@app.route('/createWallet', methods=['POST'])
+def createWallet():
+        functionName = "[createWallet]"
+        print(functionName)
         client = LibraClient() # Default client connecting to the official testnet
         wallet = LibraWallet()
 
-        data = request.get_json()
-        monny = data['monny']
+        # data = request.get_json()
+        # amount = data['amount']
 
         account = wallet.get_account(0)
-        account = client.mint_with_faucet(account, monny)
+        client.mint_with_faucet(account, 1000)
         account_state = client.get_account_state(account)
-        return {
+
+        result = {
                 "mnemonic":wallet.to_mnemonic(),
-                "authenticationKey":account_state.authentication_key,
+                "address":account_state.authentication_key,
                 "balance":account_state.balance,
                 "sequenceNumber":account_state.sequence_number,
                 "receivedEventsCount":account_state.received_events_count,
                 "sentEventsCount":account_state.sent_events_count
-        },201
+        }
 
-@app.route('/libra/mintMonnyAccount', methods=['POST'])
-def mintMonnyAccount():
+        print("result : {}".format(result))
+        return result,201
+
+@app.route('/mint', methods=['POST'])
+def mint():
+
+        functionName = "[mint]"
+        print(functionName)
         
         data = request.get_json()
         mnemonic = data['mnemonic']
-        monny = data['monny']
+        address = data['address']
+        amount = data['amount']
 
         client = LibraClient()
+
         wallet = LibraWallet(mnemonic)
         account = wallet.get_account(0)
-        client.mint_with_faucet(account, monny)
-        account_state = client.get_account_state(account)
+        account_state = client.get_account_state(address)
+        if account_state.authentication_key != address :
+                return {
+                        "massage" : "inquiryBalance fail mnemonic no relational with address",
+                        "mnemonic" : mnemonic,
+                        "address" : address
+                },400
 
-        return {
+        client.mint_with_faucet(account, amount)
+        account_state = client.get_account_state(address)
+
+
+        result = {
                 "mnemonic":mnemonic,
-                "authenticationKey":account_state.authentication_key,
+                "address":account_state.authentication_key,
                 "balance":account_state.balance,
                 "sequenceNumber":account_state.sequence_number,
                 "receivedEventsCount":account_state.received_events_count,
                 "sentEventsCount":account_state.sent_events_count
-        },200
+        }
+
+        print("result : {}".format(result))
+        return result,200
 
 
-@app.route('/libra/transfer', methods=['POST'])
+@app.route('/transfer', methods=['POST'])
 def transfer():
 
-        data = request.get_json()
-        sender = data['sender'] #sender : mnemonic == SK
-        receiver = data['receiver'] #receiver hash<hex> : PK
-        monny = data['monny'] 
+        functionName = "[transfer]"
+        print(functionName)
 
-        if monny <= 0  :
+        data = request.get_json()
+        sender = data['mnemonic'] #sender : mnemonic == SK
+        receiver = data['toAddress'] #receiver hash<hex> : PK
+        amount = data['amount'] 
+
+        if amount <= 0  :
                 return { 
-                        "massage" : "Transfer fail requiry monny >= 1 "
+                        "massage" : "Transfer fail requiry amount >= 1 "
                 },400
 
         #[EX]mnemonic : "zero job file nothing north ask gasp brush yard sand unhappy good"
@@ -69,9 +95,9 @@ def transfer():
         sender_wallet = LibraWallet(sender)
         sender_account = sender_wallet.get_account(0)
         sender_account_state = client.get_account_state(sender_account)
-        if sender_account_state.balance < monny :
+        if sender_account_state.balance < amount :
                 return {
-                        "massage" : "Transfer fail monny to transfer > now balance for wallet",
+                        "massage" : "Transfer fail amount to transfer > now balance for wallet",
                         "sender" : sender_account_state.authentication_key,
                         "now_balance" : sender_account_state.balance
                 },400
@@ -79,52 +105,66 @@ def transfer():
         sender_account = sender_wallet.get_account(0)
 
         # Create a transfer transaction object to send 0.001 Libra to account2 
-        Transaction = TransferTransaction(receiver, monny)
+        Transaction = TransferTransaction(receiver, amount)
         # Or to send to a plain hex address
 
         # You can send a transaction by calling `send_transaction` function, which takes a sender `Account` and a `Transaction` object. You can also optionally passed `max_gas_amount`, `gas_unit_price`, and `expiration_time`. 
         client.send_transaction(sender_account, Transaction)
-        return { 
+        result =  { 
                 "massage" : "Transfer success",
                 "sender" : sender_account_state.authentication_key,
                 "receiver" : receiver,
-                "monny" : monny
-        },201
+                "amount" : amount
+        }
 
-@app.route('/libra/inquiryBalance', methods=['POST'])
-def inquiryBalance():
-        # client = LibraClient()
-        # wallet = LibraWallet()
+        print("result : {}".format(result))
+        return result,201
+
+@app.route('/getBalance', methods=['POST'])
+def getBalance():
+
+        functionName = "[getBalance]"
+        print(functionName)
+     
         client = LibraClient()
         data = request.get_json()
-        
+
         mnemonic = data["mnemonic"]
-        authentication_key = data['authentication_key']
+        address = data['address']
 
         wallet = LibraWallet(mnemonic)
+        # try :
+        #         wallet = LibraWallet(mnemonic)
+        # except TypeError as error :
+        #         print("Error : {}".format(error))
+        #         return { "error" : error },400
+
         account = wallet.get_account(0)
         account_state_by_mnemonic = client.get_account_state(account)
 
-        if account_state_by_mnemonic.authentication_key != authentication_key :
+        if account_state_by_mnemonic.authentication_key != address :
                   return {
-                        "massage" : "inquiryBalance fail mnemonic no relational with authentication_key",
+                        "massage" : "inquiryBalance fail mnemonic no relational with address",
                         "mnemonic" : mnemonic,
-                        "authentication_key" : authentication_key
+                        "address" : address
                 },400
 
-        account_state = client.get_account_state(authentication_key)
+        account_state = client.get_account_state(address)
 
-        return {
-                # "mnemonic":mnemonic,
-                "authentication_key":authentication_key,
+        result = {
+                "mnemonic":mnemonic,
+                "address":address,
                 "balance":account_state.balance,
                 "sequence_number":account_state.sequence_number,
                 "received_events_count":account_state.received_events_count,
                 "sent_events_count":account_state.sent_events_count
-        },200
+        }
+
+        print("result : {}".format(result))
+        return result,200
 
 
 if __name__ == '__main__':
     # app = connexion.App(__name__, specification_dir='swagger/')
     # app.add_api('sample.yaml')
-    app.run(debug=True,port=8080)
+    app.run(host='0.0.0.0',debug=True,port=3000)
